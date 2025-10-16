bfa644a5429ac26339ecf42be05668343b199c85
salmaad <salma.ayad.02@gmail.com>
2025-10-16 10:06:57 +0100

    Customer Service

--- customer-service/src/main/java/com/ayad/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/ayad/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ayad.customerservice;
 
+import com.ayad.customerservice.entities.Customer;
+import com.ayad.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,25 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("Salma").email("salma@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Taha").email("taha@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Najlaa").email("najlaa@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(c->{
+                System.out.println("========================");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("========================");
+            });
+        };
+    }
 }
