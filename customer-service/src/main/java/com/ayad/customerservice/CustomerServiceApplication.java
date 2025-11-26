b780b3fc293b1dfb3fa60ec1e932bdb9e027a3d6
salmaad <salma.ayad.02@gmail.com>
2025-11-26 21:01:49 +0100

    config service done

--- customer-service/src/main/java/com/ayad/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/ayad/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.ayad.customerservice;
 
+import com.ayad.customerservice.config.CustomerConfigParams;
 import com.ayad.customerservice.entities.Customer;
 import com.ayad.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
---
+++ customer-service/src/main/java/com/ayad/customerservice/config/CustomerConfigParams.java
@@ -0,0 +1,8 @@
+package com.ayad.customerservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "customer.params")
+public record CustomerConfigParams(int x, int y) {
+
+}
