02fdd2371bf33239b1cc62eb52af5219e819eee1
salmaad <salma.ayad.02@gmail.com>
2025-11-27 11:15:38 +0100

    product component

--- ecom-web-app/src/app/products/products.component.ts
+++ ecom-web-app/src/app/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products : any;
+  constructor(private http:HttpClient) {
+  }
 
+  ngOnInit():void {
+    this.http.get("http://localhost:8888/inventory-service/api/products").subscribe({
+      next : (data)=>{
+        this.products=data;
+      },
+      error : (err)=>{}
+    });
+  }
 }
